56afaa16931ef6d2ecd87841c5d92e529b36ff3e
Superpol@ris <sallamy2580@gmail.com>
2021-02-18 18:23:02 -0500

    Initial commit

---
+++ unique_holders.ts
@@ -0,0 +1,67 @@
+//William Ambrozic 2022
+//Code adapted from https://solanacookbook.com/references/nfts.html#candy-machine-v1
+
+import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
+import bs58 from 'bs58';
+import axios from "axios"
+
+const connection = new Connection("https://ssc-dao.genesysgo.net");
+const MAX_NAME_LENGTH = 32;
+const MAX_URI_LENGTH = 200;
+const MAX_SYMBOL_LENGTH = 10;
+const MAX_CREATOR_LEN = 32 + 1 + 1;
+const MAX_CREATOR_LIMIT = 5;
+const MAX_DATA_SIZE = 4 + MAX_NAME_LENGTH + 4 + MAX_SYMBOL_LENGTH + 4 + MAX_URI_LENGTH + 2 + 1 + 4 + MAX_CREATOR_LIMIT * MAX_CREATOR_LEN;
+const MAX_METADATA_LEN = 1 + 32 + 32 + MAX_DATA_SIZE + 1 + 1 + 9 + 172;
+const CREATOR_ARRAY_START = 1 + 32 + 32 + 4 + MAX_NAME_LENGTH + 4 + MAX_URI_LENGTH + 4 + MAX_SYMBOL_LENGTH + 2 + 1 + 4;
+
+const TOKEN_METADATA_PROGRAM = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+const candyMachineId = new PublicKey('9yz273zB6rQHyptbSpVvC75o4G17NwJrTk4u2ZiNV3tZ');
+
+const getMintAddresses = async (firstCreatorAddress: PublicKey) => {
+  const metadataAccounts = await connection.getProgramAccounts(
+    TOKEN_METADATA_PROGRAM,
+    {
+      // The mint address is located at byte 33 and lasts for 32 bytes.
+      dataSlice: { offset: 33, length: 32 },
+
+      filters: [
+        // Only get Metadata accounts.
+        { dataSize: MAX_METADATA_LEN },
+
+        // Filter using the first creator.
+        {
+          memcmp: {
+            offset: CREATOR_ARRAY_START,
+            bytes: firstCreatorAddress.toBase58(),
+          },
+        },
+      ],
+    },
+  );
+
+  return metadataAccounts.map((metadataAccountInfo) => (
+    bs58.encode(metadataAccountInfo.account.data)
+  ));
+};
+
+(async () => {
+   console.log("Fetching Mint Addresses");
+   let arr = await getMintAddresses(candyMachineId);
+   console.log("Found " + arr.length + " mint addresses");
+   let addr = new Set();
+
+
+for (let i = 0; i < arr.length; i++) {
+   const largestAccounts = await connection.getTokenLargestAccounts(new PublicKey(arr[i]));
+   const largestAccountInfo = await connection.getParsedAccountInfo(largestAccounts.value[0].address);
+   //Fixes dead json returned from getParsedAccountInfo
+   let ded = largestAccountInfo.value!.data;
+   let buf = Buffer.from(JSON.stringify(ded));
+   console.log("CHECKING: " + JSON.parse(buf.toString()).parsed.info.owner);
+   addr.add(JSON.parse(buf.toString()).parsed.info.owner);
+ }
+
+ console.log("\nYour project has " + addr.size + " unqiue holders");
+
+})()
