649d0e163c80d902fce720dc78cbac9682750681
Superpol@ris <sallamy2580@gmail.com>
2021-02-28 21:15:48 -0500

    Update holder_distribution.ts

--- holder_distribution.ts
+++ holder_distribution.ts
@@ -3,8 +3,10 @@
 
 import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
 import bs58 from 'bs58';
+import config from './config.json';
+import * as fs from 'fs';
 
-const connection = new Connection("https://ssc-dao.genesysgo.net");
+const connection = new Connection(config.data.RPC);
 const MAX_NAME_LENGTH = 32;
 const MAX_URI_LENGTH = 200;
 const MAX_SYMBOL_LENGTH = 10;
@@ -15,7 +17,7 @@ const MAX_METADATA_LEN = 1 + 32 + 32 + MAX_DATA_SIZE + 1 + 1 + 9 + 172;
 const CREATOR_mintAddrAY_START = 1 + 32 + 32 + 4 + MAX_NAME_LENGTH + 4 + MAX_URI_LENGTH + 4 + MAX_SYMBOL_LENGTH + 2 + 1 + 4;
 
 const TOKEN_METADATA_PROGRAM = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
-const candyMachineId = new PublicKey('ENTER-YOUR-ID-HERE');
+const candyMachineId = new PublicKey(config.data.mint_id);
 
 const getMintAddresses = async (firstCreatorAddress: PublicKey) => {
   const metadataAccounts = await connection.getProgramAccounts(
@@ -46,11 +48,25 @@ const getMintAddresses = async (firstCreatorAddress: PublicKey) => {
 
 (async () => {
    console.log("Fetching Mint Addresses (this may take a few minutes, sit back)");
+   let mintAddr = [];
 
-   //Getting mint addresses from candy machine ID
-   let mintAddr = await getMintAddresses(candyMachineId);
+   //Getting mint addresses from candy machine ID or saved JSON
+   try {
+     let mint = require('./mint_addr/' + config.data.mint_id + ".json");
+     mintAddr = mint.mint_addr;
+     console.log("Found mint addresses from local JSON file");
+   } catch (error) {
+     mintAddr = await getMintAddresses(candyMachineId);
+     let save_json = {mint_addr: mintAddr};
+     if (!fs.existsSync("./mint_addr")){
+       fs.mkdirSync("./mint_addr");
+     }
+     fs.writeFile ("./mint_addr/" + config.data.mint_id + ".json", JSON.stringify(save_json), function(err:any) {
+      if (err) throw err;
+      });
+   }
+
    let mintLen = mintAddr.length;
-
    console.log("Found " + mintLen + " mint addresses");
 
    //Creating hashmap to store address counts (nfts held)
